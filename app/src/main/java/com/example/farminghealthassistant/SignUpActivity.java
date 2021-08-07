51009799d653ed4b7244c591e6a3063ded43b36f
Martin Njuguna <njuguna.maritn736@gmail.com>
2021-08-07 15:43:49 +0300

    Changed from Phone to Email authentication

--- app/src/main/java/com/example/farminghealthassistant/SignUpActivity.java
+++ app/src/main/java/com/example/farminghealthassistant/SignUpActivity.java
@@ -17,7 +17,7 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        loginbtn = findViewById(R.id.loginlink_btn);
+        loginbtn = findViewById(R.id.login_link_btn);
         loginbtn.setOnClickListener(v -> {
             Intent intent = new Intent(SignUpActivity.this, LogInActivity.class);
             startActivity(intent);
